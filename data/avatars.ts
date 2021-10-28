61043f5977adfe62717d9d6c5998de384da537c9
Nox <juan.angarita.11@gmail.com>
2021-10-28 18:06:55 -0500

    Add more attributes

--- data/avatars.ts
+++ data/avatars.ts
@@ -1,2 +1,10 @@
 import { Avatar } from "../interfaces";
-export const avatars: Avatar[] = [{ id: "1", name: "Juan" }];
+export const avatars: Avatar[] = [
+  {
+    id: "1",
+    name: "Juan",
+    image: "https://i.imgur.com/EoNjuCA.jpeg",
+    description: "Frontend Developer",
+    attributes: [{ trait_type: "Team", value: "Frontend" }],
+  },
+];
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,4 +1,12 @@
+type Attribute = {
+  trait_type: string;
+  value: string | number;
+};
+
 export type Avatar = {
   id: string;
   name: string;
+  image: string;
+  description: string;
+  attributes: Array<Attribute>;
 };
